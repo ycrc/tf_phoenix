1daba29b2fbba6e14a11e675b61dd30e90fc70c2
Tyler Trafford <tyler.trafford@yale.edu>
2026-01-29 11:44:07 -0500

    move local var definition of cloud-init data directly into data section

--- instance/userdata.tf
+++ instance/userdata.tf
@@ -1,23 +1,19 @@
-locals {
-  user_data = {
-    user                 = var.username
-    hostname             = var.fqdn
-    fqdn                 = var.fqdn
-    timezone             = "America/New_York"
-    create_hostname_file = true
-    disable_root         = true
-    ssh_pwauth           = false
-    ssh_keys             = var.ssh_host_keys
-  }
-}
-
 data "cloudinit_config" "user_data" {
   part {
     filename     = "cloud-config.yaml"
     content_type = "text/cloud-config"
     content      = <<-EOT
       #cloud-config
-      ${yamlencode(local.user_data)}
+      ${yamlencode({
+        user                 = var.username
+        hostname             = var.fqdn
+        fqdn                 = var.fqdn
+        timezone             = "America/New_York"
+        create_hostname_file = true
+        disable_root         = true
+        ssh_pwauth           = false
+        ssh_keys             = var.ssh_host_keys
+      })}
     EOT
   }
 }
